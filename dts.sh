77d5ca1490dc3a738cc7388bd4b71ed89fb28df5
pradeepvarma107 <pradeepvarma107@gmail.com>
2018-10-24 04:43:57 +0100

    Regenerate

--- dts.sh
+++ dts.sh
@@ -2,28 +2,26 @@
 
 # INSTRUCTIONS
 # ------------
-# Place S7 boot.img in this folder, rename to boot-s7.img
+# Rename dtb File That extracted From boot.img as dtb.img
 # DTS Files will be found in the Varma folder
 
 export RDIR=$(pwd)
 # Extract DTB from boot.img Varma
-$RDIR/extract-dtb.py -o $RDIR/s7 $RDIR/boot-s7.img
+$RDIR/extract-dtb.py -o $RDIR/unwanted $RDIR/dtb.img
 
 # Moving and Cleanup
 mkdir Varma
-mv -f $RDIR/s7/01_dtbdump_samsung,exynos8890.dtb $RDIR/Varma/exynos8890-herolte_eur_open_04.dtb
-mv -f $RDIR/s7/02_dtbdump_samsung,exynos8890.dtb $RDIR/Varma/exynos8890-herolte_eur_open_08.dtb
-mv -f $RDIR/s7/03_dtbdump_samsung,exynos8890.dtb $RDIR/Varma/exynos8890-herolte_eur_open_09.dtb
-mv -f $RDIR/s7/04_dtbdump_samsung,exynos8890.dtb $RDIR/Varma/exynos8890-herolte_eur_open_10.dtb
-rm -rf $RDIR/s7
+mv -f $RDIR/unwanted/01_dtbdump_samsung,exynos8890.dtb $RDIR/Varma/exynos8890-herolte_eur_open_04.dtb
+mv -f $RDIR/unwanted/02_dtbdump_samsung,exynos8890.dtb $RDIR/Varma/exynos8890-herolte_eur_open_08.dtb
+mv -f $RDIR/unwanted/03_FILE_NAME_OF_DTB.dtb $RDIR/Varma/FILE_NAME_OF_DTB.dtb
+mv -f $RDIR/unwanted/04_FILE_NAME_OF_DTB.dtb $RDIR/Varma/FILE_NAME_OF_DTB.dtb
+rm -rf $RDIR/unwanted
 
 # Convert DTB to DTS
 ./dtc -I dtb -O dts -o $RDIR/Varma/exynos8890-herolte_eur_open_04.dts $RDIR/Varma/exynos8890-herolte_eur_open_04.dtb
-./dtc -I dtb -O dts -o $RDIR/Varma/exynos8890-herolte_eur_open_08.dts $RDIR/Varma/exynos8890-herolte_eur_open_08.dtb
-./dtc -I dtb -O dts -o $RDIR/Varma/exynos8890-herolte_eur_open_09.dts $RDIR/Varma/exynos8890-herolte_eur_open_09.dtb
-./dtc -I dtb -O dts -o $RDIR/Varma/exynos8890-herolte_eur_open_10.dts $RDIR/Varma/exynos8890-herolte_eur_open_10.dtb
-./dtc -I dtb -O dts -o $RDIR/Varma/exynos8890-hero2lte_eur_open_04.dts $RDIR/Varma/exynos8890-hero2lte_eur_open_04.dtb
-./dtc -I dtb -O dts -o $RDIR/Varma/exynos8890-hero2lte_eur_open_08.dts $RDIR/Varma/exynos8890-hero2lte_eur_open_08.dtb
+./dtc -I dtb -O dts -o $RDIR/Varma/FILE_NAME_OF_DTB.dts $RDIR/Varma/FILE_NAME_OF_DTB.dtb
+./dtc -I dtb -O dts -o $RDIR/Varma/FILE_NAME_OF_DTB.dts $RDIR/Varma/FILE_NAME_OF_DTB.dtb
+
 
 # Remove DTB Files
 rm -f $RDIR/Varma/*.dtb
